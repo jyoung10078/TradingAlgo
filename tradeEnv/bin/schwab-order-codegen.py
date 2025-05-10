ca04aa0065556197db98a7fe4c5a743dc82c2b5c
Jared Young <79735672+jyoung10078@users.noreply.github.com>
2025-05-10 15:42:23 -0500

    Removing Virtual Env

--- tradeEnv/bin/schwab-order-codegen.py
+++
@@ -1,6 +0,0 @@
-#!/Users/Jared.Young/Documents/TradingAlgo/tradeEnv/bin/python3.11
-from schwab.scripts.orders_codegen import latest_order_main
-
-if __name__ == '__main__':
-    import sys
-    sys.exit(latest_order_main(sys.argv[1:]))
