8dff429a8b9243054350943480cba69fa6a0cf14
Jared Young <79735672+jyoung10078@users.noreply.github.com>
2025-05-12 01:44:22 -0500

    Setting Up MLTrader

--- backtesting.py
+++ backtesting.py
@@ -4,6 +4,7 @@ from lumibot.backtesting import YahooDataBacktesting
 from lumibot.traders import Trader
 from datetime import datetime
 from components.FirstStrategy import FirstStrategy
+from components.MLTrader import MLTrader
 from components.api_creds import ALPACA_CREDS
 
 
@@ -11,6 +12,8 @@ start_date = datetime(2024, 1, 1)
 end_date = datetime(2024, 12, 31)
 
 broker = Alpaca(ALPACA_CREDS)
+
+# Strategy Testing 1
 strategy = FirstStrategy(name='first_strategy'
                       , broker=broker
                       , parameters={"symbol": "SPY"
@@ -25,3 +28,17 @@ strategy.backtest(
                   , "cash_at_risk": 0.5}
 )
 
+# Strategy Testing 2
+strategy2 = MLTrader(name='ml_trader_strategy'
+                      , broker=broker
+                      , parameters={"symbol": "SPY"
+                                    , "cash_at_risk": 0.5}
+                      )
+
+strategy2.backtest(
+    YahooDataBacktesting
+    , backtesting_start=start_date
+    , backtesting_end=end_date
+    , parameters={"symbol": "SPY"
+                  , "cash_at_risk": 0.5}
+)
---
+++ components/MLTrader.py
@@ -0,0 +1,36 @@
+
+from lumibot.strategies import Strategy
+
+class MLTrader(Strategy):
+    """ Class for a simple strategy that buys a stock and holds it. """
+
+    def initialize(self, symbol:str="SPY", cash_at_risk:float=0.5):
+        self.symbol = symbol
+        self.sleeptime = "24H" # The amount of time to sleep between trades
+        self.last_trade = None
+        self.cash_at_risk = cash_at_risk
+
+
+    def position_sizing(self):
+        cash = self.get_cash()
+        last_price = self.get_last_price(self.symbol)
+        quantity = round((cash * self.cash_at_risk) / last_price, 0)
+        return cash, last_price, quantity
+
+
+    def on_trading_iteration(self):
+        cash, last_price, quantity = self.position_sizing()
+
+        order = None
+
+        if last_price < cash:
+            if self.last_trade == None:
+                order = self.create_order(
+                    self.symbol,
+                    quantity,
+                    "buy",
+                    type="market"
+                )
+
+                order_status = self.submit_order(order)
+                self.last_trade = "buy"
